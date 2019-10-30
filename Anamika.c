fda8a68a9c6e40f92c39cca113587c96e2e911b3
anamikasingh13 <57181865+anamikasingh13@users.noreply.github.com>
2019-10-30 18:56:38 +0530

    Update Anamika.c

--- Anamika.c
+++ Anamika.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int n, rev = 0, rem, Integer;
+    int n, rev = 0, rem, m;
     printf("Enter an integer: ");
     scanf("%d", &n);
-    Integer = n;
+    m = n;
     while( n!=0 )
     {
         rem = n%10;
         rev = rev*10 + rem;
         n =n/10;
     }
-    if (Integer == rev)
-        printf("%d is a palindrome.",Integer);
+    if (m == rev)
+        printf("%d is a palindrome",m);
     else
-        printf("%d is not a palindrome.",Integer);
+        printf("%d is not a palindrome",m);
     return 0;
 }
